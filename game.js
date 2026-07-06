7936601aaf8f443e9f25bd345fafc9073153a6da
Kyousukee <jjosemiguel.jv@gmail.com>
2026-07-06 16:28:05 -0400

    fix: prevent animation frame from running when game is paused or over

--- game.js
+++ game.js
@@ -258,7 +258,7 @@ function loop(ts) {
     }
   }
   draw();
-  animId = requestAnimationFrame(loop);
+  if (!gameOver && !paused) animId = requestAnimationFrame(loop);
 }
 
 function applyTheme(next) {
